b629e0788c930f987cfa79edd43c9ab7e1fa6de6
shah_arafat <shah@co.design>
2022-03-10 14:38:14 +0600

    day 2 year 2015 solved

--- tests/test_2015day_2.py
+++ tests/test_2015day_2.py
@@ -1,9 +1,20 @@
 import unittest
 
-from year2015.solutions.day_2 import part_1
+from year2015.solutions.day_2 import part_1, part_2
 
 
 class Part_1_test(unittest.TestCase):
     def test_1(self):
         result = part_1("year2015/inputs/day_2/part_1_test_1.txt")
         self.assertEqual(result, 58)
+
+
+class Part_2_test(unittest.TestCase):
+    def test_1(self):
+        result = part_2("year2015/inputs/day_2/part_2_test_1.txt")
+        self.assertEqual(result, 34)
+
+    def test_2(self):
+        result = part_2("year2015/inputs/day_2/part_2_test_puzzle_inp.txt")
+        print(result)
+        # self.assertEqual(result, 34)
--- year2015/solutions/day_2.py
+++ year2015/solutions/day_2.py
@@ -31,6 +31,36 @@ def part_1(input: str) -> int:
     return total_square_feet
 
 
+def part_2(input: str) -> int:
+    total_feet = 0
+    # open file
+    content = open(input)
+    count = 0
+    line = content.readline().split("x")
+    while len(line) == 3:
+        count += 1
+
+        l = int(line[0])
+        w = int(line[1])
+        h = int(line[2])
+        # make list
+        data = [l, w, h]
+        # sort
+        data.sort()
+        # make smallest perimeter with two smallest value
+        smallest_perimeter = data[0] * 2 + data[1] * 2
+
+        bow = l * w * h
+
+        total_feet += smallest_perimeter + bow
+
+        line = content.readline().split("x")
+
+    # close file
+    content.close()
+    return total_feet
+
+
 # if __name__ == "__main__":
 #     part_1("year2015/inputs/day_2/part_1_test_1.txt")
 #     part_1("year2015/inputs/day_2/part_1_test_inp.txt")
